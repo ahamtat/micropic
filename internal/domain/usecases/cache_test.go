e5c5bc8acf2b491f9dcce2e7f22b17e7121f453c
Artem Orlov <a.orlov@veedo.ru>
2020-04-22 20:25:58 +0300

    Moved memory storage implementation to cache test

--- internal/domain/usecases/cache_test.go
+++ internal/domain/usecases/cache_test.go
@@ -1,9 +1,11 @@
 package usecases
 
 import (
+	"sync"
 	"testing"
 
 	"github.com/AcroManiac/micropic/internal/domain/interfaces"
+	"github.com/pkg/errors"
 
 	"github.com/AcroManiac/micropic/internal/domain/entities"
 	"github.com/stretchr/testify/require"
@@ -112,4 +114,53 @@ func TestLRUCache_Clean(t *testing.T) {
 	// Clean cache from data
 	err := cache.Clean()
 	require.Nil(t, err, "error should be nil")
+}
+
+// MemoryStorage implementation
+type MemoryStorage struct {
+	mx   sync.RWMutex
+	data map[string][]byte
+}
+
+// NewMemoryStorage constructor
+func NewMemoryStorage() interfaces.Storage {
+	return &MemoryStorage{
+		mx:   sync.RWMutex{},
+		data: make(map[string][]byte),
+	}
+}
+
+// Save data in memory by hash key
+func (s *MemoryStorage) Save(hash string, data []byte) error {
+	s.mx.Lock()
+	defer s.mx.Unlock()
+	s.data[hash] = data
+	return nil
+}
+
+// Get stored data by hash key
+func (s *MemoryStorage) Get(hash string) ([]byte, error) {
+	s.mx.RLock()
+	defer s.mx.RUnlock()
+	data, ok := s.data[hash]
+	if !ok {
+		return nil, errors.New("error finding value")
+	}
+	return data, nil
+}
+
+// Remove data from storage
+func (s *MemoryStorage) Remove(hash string) error {
+	s.mx.Lock()
+	defer s.mx.Unlock()
+	delete(s.data, hash)
+	return nil
+}
+
+// Clean storage
+func (s *MemoryStorage) Clean() error {
+	s.mx.Lock()
+	defer s.mx.Unlock()
+	s.data = make(map[string][]byte)
+	return nil
 }
